2132c031efef8b5ca188526d2c970267de9d7713
alexis-ford <alexisford@knights.ucf.edu>
2022-04-25 20:07:23 -0400

    layout-need to fix exercise button functions

--- Option2/exerciseapp/screens/LowerBodyScreen.js
+++ Option2/exerciseapp/screens/LowerBodyScreen.js
@@ -1,23 +1,51 @@
 import { StatusBar } from "expo-status-bar"
 import { useCallback } from "react"
-import { Button, View, Text } from "react-native"
+import { Button, View, Text, FlatList, StyleSheet } from "react-native"
 import {  styles } from "../App"
 
-let exerciseList = [
+function lowerWorkout ({route, navigation}) {
+  let lowerBody =  useCallback (({ name }) => {
+    navigation.push("LowerBody", {name: "Squats", count: route.params.count})
+  })
+  return (
+    <View style={styles.container}>
+      <Text>{route.params.name} : {route.params.count}</Text>
+      <Button onPress={lowerBody} title="go to screen"></Button>
+      <StatusBar style="auto" />
+    </View>
+  )
+ }
+
+export default function LowerBodyScreen ({ route, navigation }) {
+  let lowerExercises = [
     {
       name: "Squats",
-      key: "3",
-      suggestedNextExercise: "4"
+      key: "1",
     },
     {
       name: "Lunges",
-      key: "4",
-      suggestedNextExercise: "3"
+      key: "2",
     },
   ]
-
-export default function LowerBodyScreen ({ navigation }) {
-    return (
-        <Text>Lower Body</Text>
+  let upperBody =  useCallback (({ name }) => {
+    navigation.navigate("LowerBody", {name: name, count: 0})
+  })
+  return (
+    <View style={styles.container}>
+      <Text style={exerciseStyles.head}>Choose Your First Exercise</Text>
+      <FlatList data={lowerExercises} renderItem={({ item }) =>
+        <Button title={item.name} onPress={() => lowerBody(item)}></Button>
+      }/>
+      <StatusBar style="auto" />
+  </View>
     )
 }
+
+let exerciseStyles = StyleSheet.create({
+  head: {
+      textAlign: 'center',
+      fontWeight: 'bold',
+      fontSize: 20,
+      marginBottom: 10
+  },
+})
